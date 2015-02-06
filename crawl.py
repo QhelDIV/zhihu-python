2fcee153ced275484640bef68bcd04a416724e8c
QhelDIV <yxgunbreakable@gmail.com>
2015-02-05 23:34:48 -0800

    small changes

--- crawl.py
+++ crawl.py
@@ -1,10 +1,11 @@
 # -*- coding: utf-8 -*-
 from zhihu import User
 from collections import deque
+myfile=open("DirectedGraph.txt", "w");
 #max user number “关注关系图”的节点数V
-MAXTOT=200;
-#change 'qheldiv' to your desired username，把qheldiv换成你希望的起始用户名
-firstuser_url = "http://www.zhihu.com/people/qheldiv"
+MAXTOT=100;
+#起始用户列表
+start_url = ["http://www.zhihu.com/people/chenyao-45"]
 """
 user = User(user_url)
 user_id = user.get_user_id()
@@ -21,14 +22,15 @@ asks = user.get_asks()
 answers = user.get_answers()
 collections = user.get_collections()
 """
-userque=deque()
-userque.append(User(firstuser_url))
-recorder=dict()
-total=1;
-num=1;
-flag=False;			
-with open("DirectedGraph.txt", "w") as myfile:
-	myfile.write("{")
+recorder=dict();
+userque=deque();
+for suser in start_url:
+        tuser=User(suser);
+        userque.append( tuser );
+        recorder[suser[28:]]=tuser.get_user_id();
+total=len(userque);
+num=total;
+flag=False;
 #DirectedGraph="{"
 while num>=1:
 	num-=1;
@@ -36,17 +38,21 @@ while num>=1:
 	print user.get_user_id()
 	followees_num =user.get_followees_num()
 	followees = user.get_followees()
-	for i in range(1,followees_num):
+	for i in range(1,followees_num+1):
 		cuser=followees.next()
 		# when total<=MAXTOT,then cuser will be in userque.
 		# when total> MAXTOT,we only count users who have appeard in userque.
-		if total<=MAXTOT or (total>MAXTOT and cuser.user_url in recorder):
-			with open("DirectedGraph.txt", "a") as myfile:
- 			   myfile.write('<"'+user.user_url[28:]+'","'+cuser.user_url[28:]+'">,')
+		if total<=MAXTOT or (total>MAXTOT and (cuser.user_url[28:] in recorder)):
+		        myfile.write('{"'+user.user_url[28:]+'","'+cuser.user_url[28:]+'"},')
 		if total<=MAXTOT:
 			if cuser.user_url not in recorder:
 				userque.append(cuser)
 				num+=1;
 				total+=1;
-				recorder[cuser.user_url]=True;
-#DirectedGraph=DirectedGraph[:-1]+"}"
+				recorder[cuser.user_url[28:]]=cuser.get_user_id();
+myfile.write('\n\n\n');
+st="{";
+for url in recorder:
+        st+="{0}\"{1}\",\"{2}\"{3},".format("{",str(url),str(recorder[url]),"}");
+myfile.write(st[:-1]+'}');
+
